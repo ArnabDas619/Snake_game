eabc7a3754524e659d484925c0082be2a9f8963b
Arnab Das <arnabdas.619@gmail.com>
2023-12-20 19:14:00 +0530

    Contol the snake using the directional key

--- snake.py
+++ snake.py
@@ -1,10 +1,16 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+UP = 90
+DOWN =270
+RIGHT = 0
+LEFT = 180
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for snake_seg in range(0, 3):
@@ -19,4 +25,28 @@ class Snake:
             new_x = self.segments[snake_seg - 1].xcor()
             new_y = self.segments[snake_seg - 1].ycor()
             self.segments[snake_seg].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+        else:
+            pass
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+        else:
+            pass
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+        else:
+            pass
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+        else:
+            pass
